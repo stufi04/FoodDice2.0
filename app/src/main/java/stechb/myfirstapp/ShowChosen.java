0de8f9765357e1d0389591d96c2b0e0ad49b3fbe
iangelinin <i.angelinin@gmail.com>
2015-02-24 01:02:30 +0000

    changed design

--- app/src/main/java/stechb/myfirstapp/ShowChosen.java
+++ app/src/main/java/stechb/myfirstapp/ShowChosen.java
@@ -1,5 +1,6 @@
 package stechb.myfirstapp;
 
+import android.app.Activity;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
@@ -14,7 +15,7 @@ import android.widget.TextView;
 import java.io.IOException;
 
 
-public class ShowChosen extends ActionBarActivity {
+public class ShowChosen extends Activity{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
