e23f748050742a1ccdbe0dec412249fd7948e1ac
balkanski6 <balkanski_6@abv.bg>
2016-02-02 22:22:57 +0000

    Tabs added to the last section

--- app/src/main/java/stechb/myfirstapp/Settings.java
+++ app/src/main/java/stechb/myfirstapp/Settings.java
@@ -1,9 +1,13 @@
 package stechb.myfirstapp;
 
+import android.graphics.Color;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.TabHost;
+import android.widget.TabWidget;
+import android.widget.TextView;
 
 
 public class Settings extends ActionBarActivity {
@@ -12,6 +16,42 @@ public class Settings extends ActionBarActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
+
+        final TabHost tabHost = (TabHost) findViewById(R.id.tabHost2);
+
+        tabHost.setup();
+
+        TabHost.TabSpec tabSpec = tabHost.newTabSpec("search");
+        tabSpec.setContent(R.id.searchTab);
+        tabSpec.setIndicator("Search");
+        tabHost.addTab(tabSpec);
+
+        tabSpec = tabHost.newTabSpec("IngredientsList");
+        tabSpec.setContent(R.id.listTab);
+        tabSpec.setIndicator("Ingredients List");
+        tabHost.addTab(tabSpec);
+
+        for (int i = 0; i < 2; i++) {
+            TextView tv = (TextView) tabHost.getTabWidget().getChildAt(i).findViewById(android.R.id.title);
+            tv.setTextColor(Color.parseColor("#0D4D4D"));
+            if (i == 0) tv.setShadowLayer(10, 1, 1, Color.WHITE);
+        }
+
+        tabHost.setOnTabChangedListener(new TabHost.OnTabChangeListener() {
+
+            @Override
+            public void onTabChanged(String tabId) {
+
+                for (int i = 0; i < 2; i++) {
+                    TextView tv = (TextView) tabHost.getTabWidget().getChildAt(i).findViewById(android.R.id.title);
+                    tv.setShadowLayer(0,0,0,Color.WHITE);
+                }
+
+                TextView tv = (TextView) tabHost.getTabWidget().getChildAt(tabHost.getCurrentTab()).findViewById(android.R.id.title);
+                tv.setShadowLayer(10,1,1,Color.WHITE);;
+
+            }
+        });
     }
 
 
